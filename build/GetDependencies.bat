25509b85d28a0ff536bc27478cc998ec2ef78779
JolynWong <shiroyami08@gmail.com>
2022-07-27 11:03:18 +0800

    Bat file update

--- build/GetDependencies.bat
+++ build/GetDependencies.bat
@@ -17,28 +17,29 @@ powershell write-host -fore White ----------------------------------------------
 powershell write-host -fore White CS396_A2 - DOWNLOADING DEPENDENCIES
 powershell write-host -fore White ------------------------------------------------------------------------------------------------------
 
-REM echo.
-REM rmdir "../dependencies/xecs" /S /Q
-REM git clone https://github.com/LIONant-depot/xECS.git "../dependencies/xecs"
-REM if %ERRORLEVEL% GEQ 1 goto :ERROR
+echo.
+rmdir "../dependencies/xecs" /S /Q
+git clone https://github.com/LIONant-depot/xECS.git "../dependencies/xecs"
+if %ERRORLEVEL% GEQ 1 goto :ERROR
 
-REM cd ../dependencies/xecs
-REM git checkout Lesson09_Prefabs
-REM if %ERRORLEVEL% GEQ 1 goto :ERROR
+cd ../dependencies/xecs
+git checkout Lesson09_Prefabs
+if %ERRORLEVEL% GEQ 1 goto :ERROR
 
-REM cd build
-REM call GetDependencies.bat "return"
-REM if %ERRORLEVEL% GEQ 1 goto :ERROR
-REM cd /d %CS396_A2_PATH%
+cd build
+call GetDependencies.bat "return"
+if %ERRORLEVEL% GEQ 1 goto :ERROR
+cd /d %CS396_A2_PATH%
 
-REM rmdir "../dependencies/freeglut" /S /Q
-REM git clone https://github.com/FreeGLUTProject/freeglut.git "../dependencies/freeglut"
-REM if %ERRORLEVEL% GEQ 1 goto :PAUSE
+rmdir "../dependencies/freeglut" /S /Q
+git clone https://github.com/FreeGLUTProject/freeglut.git "../dependencies/freeglut"
+if %ERRORLEVEL% GEQ 1 goto :PAUSE
 
-REM echo.
-REM rmdir "../dependencies/freeglut_lib_bin" /S /Q
-REM git clone https://github.com/hotaru08/freeglut_lib_bin.git "../dependencies/freeglut_lib_bin"
-REM if %ERRORLEVEL% GEQ 1 goto :PAUSE
+echo.
+rmdir "../dependencies/freeglut_lib_bin" /S /Q
+git clone https://github.com/hotaru08/freeglut_lib_bin.git "../dependencies/freeglut_lib_bin"
+if %ERRORLEVEL% GEQ 1 goto :PAUSE
+
 
 :GENERATE_PROJECT
 powershell write-host -fore White ------------------------------------------------------------------------------------------------------
